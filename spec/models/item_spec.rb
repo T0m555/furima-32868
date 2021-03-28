14a6628eec106e164225e8da134e3a5b616dd0bc
T0m555 <77041875+T0m555@users.noreply.github.com>
2021-03-28 18:00:39 +0900

    修正：単体テスト追加
    半角数字でないと登録できないテストの追加。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,6 +62,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it '価格が全角数字では登録できない'  do
+        @item.price = "１５００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
