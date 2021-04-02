013e1f967a93769c80020f811c44175705e5755e
T0m555 <77041875+T0m555@users.noreply.github.com>
2021-04-02 15:16:41 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :ensure_user, only: [:update, :edit]
  
   def index
     @items = Item.order(id: :DESC)
@@ -22,9 +23,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
+
+  def ensure_user
+    @items = current_user.items
+    @item = @items.find_by(id: params[:id])
+    if @item == nil
+      redirect_to root_path
+    end
+  end
+
 end
 
