581f00f73b9e269ca3b4666b912e4b821dc5f598
T0m555 <77041875+T0m555@users.noreply.github.com>
2021-04-04 19:33:52 +0900

    payjpインストール

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
+  with_options presence: true do
+    validates :post_number, :municipality, :address, :phone_number
+    validates :prefecture_id, numericality: { other_than: 1 }
+  end
   has_one :payment
 end
