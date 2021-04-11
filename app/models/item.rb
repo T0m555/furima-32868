81bbab04800b8b82ee9164c93e7aedff5d78ddc6
T0m555 <77041875+T0m555@users.noreply.github.com>
2021-04-11 16:44:16 +0900

    修正
    バリデーションの書き方修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one :payment
   has_one_attached :image
   with_options presence: true do
-    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9_999_999 }
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :name, :description, :image
     with_options numericality: { other_than: 1 } do
       validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id
